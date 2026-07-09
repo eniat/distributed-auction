6be341a6e31bf24a1f48477dbc11901780d73286
eniat <213285296+eniat@users.noreply.github.com>
2026-07-09 12:23:10 +0100

    Refactor comments in FrontEndServer.java

--- part2/src/main/java/frontend/FrontEndServer.java
+++ part2/src/main/java/frontend/FrontEndServer.java
@@ -6,8 +6,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-// No need to make any changes to this file
-// It registers the FrontEnd with rmiregistry and starts the gRPC service
+// Registers the FrontEnd with rmiregistry and starts the gRPC service
 public class FrontEndServer {
     public static void main(String[] args) throws Exception {
         FrontEndImpl svc = new FrontEndImpl();
@@ -17,7 +16,7 @@ public class FrontEndServer {
         reg.rebind("FrontEnd", stub);
         System.out.println("FrontEndAdmin bound as 'FrontEnd'");
 
-        // Start gRPC (do not change the port number, keep it 50055)
+        // Start gRPC
         Server s = ServerBuilder.forPort(50055).addService(svc).build();
         s.start();
         System.out.println("FrontEnd started (RMI: FrontEnd, gRPC:50055)");
